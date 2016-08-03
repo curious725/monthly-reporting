33b05ad6061c957134d826759ed8b7541b0fe8cf
Charlottesometimes <m.potashykova@gmail.com>
2016-08-03 19:08:59 +0300

    probably fixed

--- db/migrate/20150616182454_change_status_in_vacation_requests.rb
+++ db/migrate/20150616182454_change_status_in_vacation_requests.rb
@@ -1,9 +1,10 @@
 class ChangeStatusInVacationRequests < ActiveRecord::Migration
   def up
-    change_column :vacation_requests, status: :integer, default: 0
+    change_column :vacation_requests, :status, :integer
+    change_column_default :vacation_requests, :status, 0
   end
 
   def down
-    change_column :vacation_requests, status: :string
+    change_column :vacation_requests, :status, :string
   end
 end
