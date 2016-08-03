c8ecd5621141d62219ef157a7666a24a4d0dc70d
Charlottesometimes <m.potashykova@gmail.com>
2016-08-03 18:59:41 +0300

    mg fixed

--- db/migrate/20150616182454_change_status_in_vacation_requests.rb
+++ db/migrate/20150616182454_change_status_in_vacation_requests.rb
@@ -1,9 +1,9 @@
 class ChangeStatusInVacationRequests < ActiveRecord::Migration
   def up
-    change_column :vacation_requests, :status, :integer, default: 0
+    change_column :vacation_requests, status: :integer, default: 0
   end
 
   def down
-    change_column :vacation_requests, :status, :string
+    change_column :vacation_requests, status: :string
   end
 end
