8a2ebac5eafa0c96076740f69cb65a9d30278b76
Charlottesometimes <m.potashykova@gmail.com>
2016-08-03 20:39:35 +0300

    report add and edit forms fixed

--- app/assets/javascripts/views/report_form.js
+++ app/assets/javascripts/views/report_form.js
@@ -39,12 +39,9 @@ App.Views.ReportForm = App.Views.BootstrapModal.extend({
   },
 
   getFormValues: function() {
-    var textarea = $('textarea');
-    // this.model.set(textarea.name, textarea.value.trim());
-    this.model.set(this.$('input[type=text]').name, this.$('input[type=text]').value.trim());
-    // _.each(this.$('input[type=text]'), function(input) {
-    //   this.model.set(input.name, input.value.trim());
-    // }, this);
+    _.each(this.$('[data-type=report-part]'), function(input) {
+      this.model.set(input.name, input.value.trim());
+    }, this);
   },
 
   setFormValues: function() {
