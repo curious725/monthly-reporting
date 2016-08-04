72d1b4d6b23fbc1f5e6602fc07112440f19dd8c1
Charlottesometimes <m.potashykova@gmail.com>
2016-08-04 17:53:11 +0300

    send email with attachments

--- app/mailers/bookkeeper_mailer.rb
+++ app/mailers/bookkeeper_mailer.rb
@@ -1,11 +1,12 @@
 class BookkeeperMailer < ActionMailer::Base
 
-  def send_report_to_bookkeeper
-    bookkeeper = 'm.potashnykova@gmail.com' 
+  def send_report_to_bookkeeper(bookkeeper)
+    @bookkeeper = 'm.potashnykova@gmail.com' 
+    @bookkeeper = bookkeeper
   	#reports_csv = Report.download_reports
   	#attachments[reports_csv] = month_report
   	
-  	mail(to: bookkeeper, subject: 'Cybercraft month report')
+  	mail(to: @bookkeeper.email, subject: 'Cybercraft month report')
    #   do |format|
   	# 	format.html { render layout: 'send_report_to_bookkeeper' }
   	# 	format.text
