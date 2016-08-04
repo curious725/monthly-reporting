b47f49cedc4f5516ffba05db1e8ce9f5ba13e9da
Charlottesometimes <m.potashykova@gmail.com>
2016-08-04 17:42:43 +0300

    send email with attachments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,9 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: 'http://cybercraft-vacations.herokuapp.com' }
+  #changed by me
+  #config.action_mailer.default_url_options = { host: 'http://cybercraft-vacations.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'https://secret-river-38516.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     tls: true,
