7203a33b5a2cc6f076ab222d0ee339caff9c0ab0
Charlottesometimes <m.potashykova@gmail.com>
2016-08-05 22:08:50 +0300

    monthly scope added

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,11 +1,18 @@
 class Report < ActiveRecord::Base
 
   belongs_to :user
-  
+
+  scope :this_months_report, -> do
+    where("created_at >= ? AND created_at <= ?", Time.zone.now.beginning_of_month, Time.zone.now.end_of_month)
+  end
+
+  # def self.this_months_report
+  #   where("created_at >= ? AND created_at <= ?", Time.zone.now.beginning_of_month, Time.zone.now.end_of_month)
+  # end
   #validates :body, length: { minimum: 20}
   #csv
   def self.to_csv(user_id=nil)
-  	reports = user_id.nil? ? all : where(user_id: user_id)
+  	reports = user_id.nil? ? this_months_report : where(user_id: user_id).this_months_report
   	reports = reports.to_a
   	#reports = where(user_id: user_id).to_a
   	CSV.generate do |csv|
